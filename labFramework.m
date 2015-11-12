bbdfd2ec39966c2b7dd4a87becdea09ac77ed629
jeharrington4 <Jeharrington4@gmail.com>
2015-11-12 12:20:59 -0500

    More testing
    some new comments were added

--- labFramework.m
+++ labFramework.m
@@ -1,6 +1,6 @@
 clc, clear
-a=arduino_sim();
-%a=arduino(‘COM#’)
+a=arduino_sim();%this is for the sim
+%a=arduino('COM#')%uncomment for real train
     %connect the Arduino to MATLAB
     %replace the # with a digit such as 3. The port will never be 1
 
@@ -18,11 +18,11 @@ rLED = 14;
 lLED = 15;
 a.pinMode(rLED,'output')%0@  right light
 a.pinMode(lLED,'output')%@0  left light
-    %set up a connection to the LED crossing lights
+    %set up a connection to the LED crossing
     %Issued once perindividual LED.
 
 a.motorRun(1,'forward')%train will go forward when powered
-
+%The train will only go one way
 %a.motorSpeed(1,X)
     %control the speed of the train
     %X=0 will stop the train
